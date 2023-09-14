5d72d84ff8bba21e9e13996caae3f7818d998b7f
Benjamin Sam <79420331+bvsam@users.noreply.github.com>
2023-09-14 15:26:28 -0400

    Making minor change to padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col p-5">
       {/* Main Content */}
-      <main className="grow container mx-auto px-6">
+      <main className="grow container mx-auto px-12">
         <div className="flex flex-col items-center">
           <Hero />
         </div>
