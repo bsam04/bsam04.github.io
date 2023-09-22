a16f975d4c331d2364ff1e90edd9bc2083c3981d
Benjamin Sam <79420331+bvsam@users.noreply.github.com>
2023-09-22 00:50:28 -0400

    Updating layout and styles of About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,10 +4,11 @@ export default function About() {
   return (
     <section>
       <h3 className="text-4xl font-bold mb-8">About</h3>
-      <div className="text-lg">
+      <div className="text-xl">
+        <p className="mb-4">Hello! I&apos;m Benjamin.</p>
         <p className="mb-4">
-          Hello! I&apos;m Benjamin. I have interests in machine learning (ML),
-          web development, and software development in general.
+          I have interests in machine learning (ML), web development, and
+          software development in general.
         </p>
         <p className="mb-4">
           Below are some of my projects. Feel free to check out my{" "}
